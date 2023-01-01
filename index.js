9430a5c47a2b5057fffa7fd8405e2bbeb83cafb2
mishuk09 <mishukinfo09@gmail.com>
2023-01-01 10:38:54 -0500

    first js code

--- index.js
+++ index.js
@@ -4,3 +4,23 @@
  function showMsg(){
     alert("Assalamu alaikum");
  }
+
+
+
+
+//  let salam=document.querySelector('button2');
+//  salam.addEventListener('click',input);
+
+//  function input(){
+//     let name=prompt('Enter name');
+//     salam.textContent='Name:Mishuk'+ name;
+//  }
+
+
+// document.write("Mahadi Hasan  mishuk");
+
+var name="Mahadi Hasan Mishuk";
+document.write(name)
+
+var num="hfdjhgj"
+parseInt(num)
