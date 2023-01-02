5d1709b623c0853c6358f48f85f9c3fab1ff7142
mishuk09 <mishukinfo09@gmail.com>
2023-01-02 12:27:10 -0500

    python lab practice

---
+++ Lab_practice/02 Jan 2023/index.js
@@ -0,0 +1,55 @@
+//string library function
+
+//   var name="Bangladesh";
+//   document.write("Number of length="+ name.length);
+//   var result=name.charAt(2);
+//   document.write(result);
+
+//   var result1=name.toUppercase();
+//   document.write(result1)
+
+
+// var name=prompt("Enter your name");
+// document.write(name.length);
+
+
+// var name=prompt("Enter your name:");
+
+// document.write(name);
+ 
+
+// var name="Bangladesh";
+// var name2="Is a beautyfull country";
+
+// var result=name.concat(name2);
+// document.write(result);
+
+
+
+// Arithmetic and Assignment operator
+
+//var num1=10;
+//var num2=20;
+//document.write("Sum of two number="+ (num1 + num2) + "<br/>");
+//document.write("Sub of two number="+ (num1 - num2) + "<br/>");
+//document.write("Mul of two number="+ (num1 * num2) + "<br/>");
+ 
+//document.write("Div of two number="+ (num1 / num2));
+
+
+
+var num1=prompt("Enter number=");
+var num2=prompt("Enter your number:");
+
+num1=parseInt(num1,10);
+num2=parseInt(num2,10);
+
+var sub,sum;
+
+sum=num1+ num2;
+sub=num1-num2;
+
+document.write("The sum is:" + sum + "<br/>");
+document.write("The sub is:" + sub);
+
+ 
