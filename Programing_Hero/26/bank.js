3db2495cb251ee2628d6c3df42031c1718471b04
mishuk09 <mishukinfo09@gmail.com>
2023-05-07 11:11:23 -0400

    JS practice

--- Programing_Hero/26/bank.js
+++ Programing_Hero/26/bank.js
@@ -1,6 +1,7 @@
 document.getElementById('loginButton').addEventListener('click', function() {
     const emailFeild = document.getElementById('userEmail');
     const userEmail = emailFeild.value;
+
     const passwordFeild = document.getElementById('userPassword');
     const userPassword = passwordFeild.value;
 
---
+++ Programing_Hero/26/bankDetils.js
@@ -0,0 +1,19 @@
+document.getElementById('dipositeButton').addEventListener('click', function() {
+    const enterAmount = document.getElementById('dipositeAmount');
+    const result = dipositeAmount.value;
+    console.log(result);
+
+    // const diposite = document.getElementById('showDiposite');
+    // console.log(diposite.innerText);
+
+
+
+
+})
+
+
+document.getElementById('withdrawButton').addEventListener('click', function() {
+    const withdrawInput = document.getElementById('withdrawAmount');
+    const result = withdrawAmount.value;
+    console.log(result);
+})
