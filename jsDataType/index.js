18381ac4eebb153c65fa3ee627bf12e74a4c22be
mishuk09 <mishukinfo09@gmail.com>
2023-08-04 22:05:21 +0530

    flutter project

---
+++ jsDataType/index.js
@@ -0,0 +1,15 @@
+// const a = 33;
+// console.log(typeof a);
+
+// const b='mahadi hasan Mishuk';
+// console.log(typeof b);
+// let a=false;
+// console.log(typeof a); 
+const a ="10";
+if (a) {
+    console.log("True");
+
+}
+else {
+    console.log("False");
+}
